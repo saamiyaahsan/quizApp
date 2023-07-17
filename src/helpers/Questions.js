913d9cecdb1c55484693e372df36d44ccb403aeb
Saamiya Ahsan <saamiyaa835@gmail.com>
2023-07-17 16:08:18 +0530

    done

---
+++ src/helpers/Questions.js
@@ -0,0 +1,45 @@
+export const Questions = [
+  {
+    prompt: "Which of the following doesn't comes under OOPS?",
+    optionA: "Abstration",
+    optionB: "Procedural Programming",
+    optionC: "Encapsulation",
+    optionD: "Polymorphism",
+    asnwer: "optionB",
+  },
+  {
+    prompt: "Which of this is not a programming language?",
+    optionA: "Python",
+    optionB: "JavaScript",
+    optionC: "MC-03",
+    optionD: "Java",
+    asnwer: "optionC",
+  },
+  {
+    prompt: "Which of this is not a javascript framework?",
+    optionA: "React",
+    optionB: "Angular",
+    optionC: "Vue",
+    optionD: "Java",
+    asnwer: "optionD",
+  },
+
+  {
+    prompt: "Who is the CEO of Google?",
+    optionA: "Sundar Pichai",
+    optionB: "Satya Nadella",
+    optionC: "Tim Cook",
+    optionD: "None of the above",
+    asnwer: "optionA",
+  },
+
+  {
+    prompt: "Who is the founder of Microsoft?",
+    optionA: "C.V. Raman",
+    optionB: "Steve Henry",
+    optionC: "Andrew Wilson",
+    optionD: "Bill Gates",
+    asnwer: "optionD",
+  },
+  
+];
